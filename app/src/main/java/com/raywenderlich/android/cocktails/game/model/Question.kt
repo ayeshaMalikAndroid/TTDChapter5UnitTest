fdca3d357ca1bc117396f62d0a387fd815c8a19d
amalik <amalik@stellatechnology.com>
2023-07-21 14:47:48 +0500

    Now make the test case pass by returning true

--- app/src/main/java/com/raywenderlich/android/cocktails/game/model/Question.kt
+++ app/src/main/java/com/raywenderlich/android/cocktails/game/model/Question.kt
@@ -30,11 +30,15 @@
 
 package com.raywenderlich.android.cocktails.game.model
 
-class Question(val correctOption: String,
-               val incorrectOption: String) {
+class Question(
+    val correctOption: String,
+    val incorrectOption: String
+) {
     var answeredOption: String? = null
         private set
-    fun answer(option : String){
-answeredOption = option
+
+    fun answer(option: String) :Boolean {
+        answeredOption = option
+        return true
     }
 }
