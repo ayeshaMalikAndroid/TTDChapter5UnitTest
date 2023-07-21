e677380d9e825d2b476b44e10f5253df22199739
amalik <amalik@stellatechnology.com>
2023-07-21 14:50:07 +0500

    write test case for incorrect option and it's fail

--- app/src/test/java/com/raywenderlich/android/cocktails/game/model/QuestionUnitTests.kt
+++ app/src/test/java/com/raywenderlich/android/cocktails/game/model/QuestionUnitTests.kt
@@ -47,4 +47,11 @@ class QuestionUnitTests {
         val result = question.answer("CORRECT")
         Assert.assertTrue(result)
     }
+
+    @Test
+    fun whenAnswering_withIncorrectOption_shouldReturnFalse() {
+        val question = Question("CORRECT", "INCORRECT")
+        val result = question.answer("INCORRECT")
+        Assert.assertFalse(result)
+    }
 }
